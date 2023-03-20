86469d1e6db4f55c643c51fabe136d121d865dbb
Daniela Meogrossi <daniela.meogrossi@gmail.com>
2023-03-20 12:18:43 +0100

    test

--- src/app/auth/AuthInterceptor.ts
+++ src/app/auth/AuthInterceptor.ts
@@ -6,9 +6,9 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
-const TOKEN_HEADER_KEY = "Authorization"; // for Spring Boot back-end @Injectable()
+
 export class AuthInterceptor implements HttpInterceptor {
-    
+
     constructor() { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = localStorage.getItem('token') || '';
@@ -20,6 +20,4 @@ export class AuthInterceptor implements HttpInterceptor {
         console.log("http request", httpRequest); 
         return next.handle(httpRequest);
     }
-
 }
-    
